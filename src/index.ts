6f23bcdb05d4514b53b0f6fa5337bd8da99511fd
Pawel Sroczyk <psroczyk96@gmail.com>
2024-08-28 17:42:19 +0200

    Implementation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,163 @@
-export type ServiceYear = 2020 | 2021 | 2022;
-export type ServiceType = "Photography" | "VideoRecording" | "BlurayPackage" | "TwoDayEvent" | "WeddingSession";
+import { ServiceDependencies, ServicePackage, ServicePackagePricing, ServicePricing, ServiceType, ServiceYear } from "./types";
+
+const basePrices: ServicePricing = {
+  "2020": {
+    Photography: { price: 1700 },
+    VideoRecording: { price: 1700 },
+    WeddingSession: { price: 600 },
+    BlurayPackage: { price: 300 },
+    TwoDayEvent: { price: 400 },
+  },
+  "2021": {
+    Photography: { price: 1800 },
+    VideoRecording: { price: 1800 },
+    WeddingSession: { price: 600 },
+    BlurayPackage: { price: 300 },
+    TwoDayEvent: { price: 400 },
+  },
+  "2022": {
+    Photography: { price: 1900 },
+    VideoRecording: { price: 1900 },
+    WeddingSession: { price: 600 },
+    BlurayPackage: { price: 300 },
+    TwoDayEvent: { price: 400 },
+  },
+};
+
+const packagePrices: ServicePackagePricing = { //TODO: this obviously could be changed to other discount-related info which will omit duplicates/use greater discount
+  "2020": {
+    Photography_VideoRecording: { price: 2200 },
+    WeddingSession_Photography: { price: 2000 },
+    WeddingSession_VideoRecording: { price: 2000 },
+    WeddingSession_VideoRecording_Photography: { price: 2500 },
+  },
+  "2021": {
+    Photography_VideoRecording: { price: 2300 },
+    WeddingSession_Photography: { price: 2100 },
+    WeddingSession_VideoRecording: { price: 2100 },
+    WeddingSession_VideoRecording_Photography: { price: 2600 },
+  },
+  "2022": {
+    Photography_VideoRecording: { price: 2500 },
+    WeddingSession_Photography: { price: 1900 },
+    WeddingSession_VideoRecording: { price: 2200 },
+    WeddingSession_VideoRecording_Photography: { price: 2500 },
+  },
+};
+
+const requiredServices: ServiceDependencies = {
+  BlurayPackage: ["VideoRecording"],
+  Photography: [],
+  TwoDayEvent: ["Photography", "VideoRecording"],
+  VideoRecording: [],
+  WeddingSession: [],
+};
 
 export const updateSelectedServices = (
-    previouslySelectedServices: ServiceType[],
-    action: { type: "Select" | "Deselect"; service: ServiceType }
-) => [];
+  previouslySelectedServices: ServiceType[],
+  action: { type: "Select" | "Deselect"; service: ServiceType }
+) => {
+  const selectedServices = [...previouslySelectedServices];
 
-export const calculatePrice = (selectedServices: ServiceType[], selectedYear: ServiceYear) => ({ basePrice: 0, finalPrice: 0 });
+  switch (action.type) {
+    case "Select":
+      const required = requiredServices[action.service];
+      if (
+        required.length > 0 &&
+        !selectedServices.some((selected) => required.includes(selected))
+      )
+        break;
+      if (!selectedServices.includes(action.service))
+        selectedServices.push(action.service);
+      return selectedServices;
+
+    case "Deselect":
+      if (!selectedServices.includes(action.service)) return selectedServices;
+
+      let updatedServices = selectedServices.filter(
+        (ss) => ss !== action.service
+      );
+      const dependentServices = Object.keys(requiredServices).filter(
+        (service) =>
+          requiredServices[service as ServiceType].includes(action.service)
+      ) as ServiceType[];
+
+      var hasOtherRequiredServices =
+        dependentServices !== null && dependentServices.length > 0
+          ? requiredServices[dependentServices[0]]
+              .filter((req) => req !== action.service)
+              .filter((req) => updatedServices.includes(req)).length > 0
+          : false;
+      if (hasOtherRequiredServices) {
+        return updatedServices;
+      }
+
+      return updatedServices.filter(
+        (service) => !dependentServices.includes(service)
+      );
+
+    default:
+      return selectedServices;
+  }
+
+  return selectedServices;
+};
+
+export const calculatePrice = (
+  selectedServices: ServiceType[],
+  selectedYear: ServiceYear
+) => {
+  let totalBasePrice = 0;
+  let finalPrice = 0;
+
+  selectedServices.forEach((selectedService) => {
+    totalBasePrice =
+      totalBasePrice + basePrices[selectedYear][selectedService].price;
+  });
+
+  const packages = getPackages(selectedServices);
+  if (packages.length > 0)
+    packages.forEach((pack) => {
+      finalPrice = finalPrice + packagePrices[selectedYear][pack].price;
+    });
+
+  return {
+    basePrice: totalBasePrice,
+    finalPrice: packages.length > 0 ? finalPrice : totalBasePrice,
+  };
+};
+
+function getPackages(selectedServices: ServiceType[]) {
+  let packages: ServicePackage[] = [];
+  if (
+    selectedServices.includes("Photography") &&
+    selectedServices.includes("VideoRecording") &&
+    selectedServices.includes("WeddingSession")
+  ) {
+    packages.push("WeddingSession_VideoRecording_Photography");
+    return packages;
+  }
+
+  if (
+    selectedServices.includes("Photography") &&
+    selectedServices.includes("VideoRecording")
+  ) {
+    packages.push("Photography_VideoRecording");
+  }
+
+  if (
+    selectedServices.includes("Photography") &&
+    selectedServices.includes("WeddingSession")
+  ) {
+    packages.push("WeddingSession_Photography");
+  }
+
+  if (
+    selectedServices.includes("VideoRecording") &&
+    selectedServices.includes("WeddingSession")
+  ) {
+    packages.push("WeddingSession_VideoRecording");
+  }
+
+  return packages;
+}
---
+++ src/types.ts
@@ -0,0 +1,24 @@
+export type ServiceYear = 2020 | 2021 | 2022;
+export type ServiceType =
+  | "Photography"
+  | "VideoRecording"
+  | "BlurayPackage"
+  | "TwoDayEvent"
+  | "WeddingSession";
+
+export type ServicePackage =
+  | "Photography_VideoRecording"
+  | "WeddingSession_Photography"
+  | "WeddingSession_VideoRecording"
+  | "WeddingSession_VideoRecording_Photography";
+
+  export interface ServicePrice {
+  price: number;
+}
+
+export type YearlyServicePrice = Record<ServiceType, ServicePrice>;
+type YearlyServicePackagesPrice = Record<ServicePackage, ServicePrice>;
+
+export type ServicePricing = Record<ServiceYear, YearlyServicePrice>;
+export type ServicePackagePricing = Record<ServiceYear, YearlyServicePackagesPrice>;
+export type ServiceDependencies = Record<ServiceType, ServiceType[]>;
